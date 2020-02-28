db1bcaca292ba4bacba55bece671313bbc8beef6
Tshering Lama <lamatshering23@gmail.com>
2020-02-28 12:19:13 +0545

    updates

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = new express();
 
+
 app.use(express.static('public'));
-//app.use(expressEdge);
+app.use(expressEdge.engine);
+
 app.set('views', __dirname + '/views');
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// });
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
 });
 
 app.get('/about', (req, res) => {
