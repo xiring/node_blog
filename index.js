de7287a3164c3d1851bf634a61b2589c1451bf77
Tshering Lama <lamatshering23@gmail.com>
2020-02-28 14:34:23 +0545

    updates

--- index.js
+++ index.js
@@ -53,11 +53,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
-// });
-
 const storePost = require('./middleware/storePost')
 const auth = require("./middleware/auth");
 const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
@@ -74,54 +69,6 @@ app.post("/users/login", redirectIfAuthenticated, loginUserController);
 app.get("/auth/register", redirectIfAuthenticated, createUserController);
 app.post("/users/register", redirectIfAuthenticated, storeUserController);
 app.get("/auth/logout", redirectIfAuthenticated, logoutController);
-
-// app.get('/', async (req, res) => {
-//     const posts = await Post.find({})
-//     res.render('index', {
-//         posts
-//     })
-// });
-
-// app.get('/posts/new', (req, res) => {
-//     res.render('create')
-// });
-
-// const Post = require('./database/models/Post');
-
-
-// app.post('/posts/store', (req, res) => {
-//     const {
-//         image
-//     } = req.files
-
-//     image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-//         Post.create({
-//             ...req.body,
-//             image: `/posts/${image.name}`
-//         }, (error, post) => {
-//             res.redirect('/');
-//         });
-//     })
-// });
-
-// app.get('/post/:id', async (req, res) => {
-//     const post = await Post.findById(req.params.id)
-//     res.render('post', {
-//         post
-//     })
-// });
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'pages/about.html'));
-// });
- 
-// app.get('/contact', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-// });
- 
-// app.get('/post', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-// });
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
